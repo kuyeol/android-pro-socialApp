88c33bbcf53979be6df0840158bbb8cb4dd1a382
kuyeol <zimop12@gmail.com>
2025-02-19 14:22:49 +0900

    sss

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MyAppliction.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import android.app.Application
+
+@HiltAndroidApp
+class MyAppliction : Application(){
+
+  override fun onCreate() {
+    super.onCreate()
+  }
+
+
+}
